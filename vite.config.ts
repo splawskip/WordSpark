1d44accb97a3f752c029809a5f2fc43d916c7b1f
Przemysław Spławski <psplawski.dev@gmail.com>
2023-08-20 17:40:47 +0200

    PostCSS Autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import autoprefixer from 'autoprefixer';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}), // add options if needed
+      ],
+    },
+  },
 });
