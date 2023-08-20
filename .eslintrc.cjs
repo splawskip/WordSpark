ed8cd0bf159d30dc815a6528d5156ac416fd3de6
Przemysław Spławski <psplawski.dev@gmail.com>
2023-08-20 17:40:47 +0200

    PostCSS Autoprefixer

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
+    project: ['./tsconfig.json', './tsconfig.node.json', './postcss.config.js'],
   },
   plugins: [
     'react',
