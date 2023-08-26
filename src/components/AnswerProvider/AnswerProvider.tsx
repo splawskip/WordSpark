9c64fe3e57b17fe1a50f44c0a1d2c87b34c87f3c
Przemysław Spławski <psplawski.dev@gmail.com>
2023-08-26 19:50:21 +0200

    Handle errors

--- src/components/AnswerProvider/AnswerProvider.tsx
+++ src/components/AnswerProvider/AnswerProvider.tsx
@@ -2,10 +2,17 @@
 import React from 'react';
 // Hooks.
 import useSWR from 'swr';
-
+// Styles.
+import styles from './AnswerProvider.module.css';
+// Expose context.
 export const AnswerContext = React.createContext<string>('');
-
-const fetcher = async (url:string) => {
+/**
+ * Calls given endpoint and fetches the data.
+ *
+ * @param url - string - URL that will be called.
+ * @returns responseJson - Promise<AnswerResponse>.
+ */
+const fetcher = async (url:string): Promise<AnswerResponse> => {
   // Call Rapid API.
   const response = await fetch(url, {
     method: 'GET',
@@ -16,13 +23,28 @@ const fetcher = async (url:string) => {
   });
   // Get as JSON.
   const responseJson = await response.json();
+  // Throw JSON if something went wrong.
+  if (!response.ok) {
+    throw responseJson;
+  }
   // Show it to the world.
   return responseJson;
 };
 
 function AnswerProvider({ children } : ChildrenOnly) {
   // Get data.
-  const { data } : AnswerResponse = useSWR('https://wordle-answers-solutions.p.rapidapi.com/today', fetcher);
+  const { data, error } = useSWR('https://wordle-answers-solutions.p.rapidapi.com/today', fetcher);
+  // Bail app if sresponsewrong.
+  if (error) {
+    return (
+      <p className={styles.error}>
+        Something&apos;s gone wrong!
+        Please let me know about this 👉
+        {' '}
+        <a title="WordSpark issues" className={styles.link} href="https://github.com/splawskip/WordSpark/issues" rel="noopener noreferrer">here</a>
+      </p>
+    );
+  }
   // Show it to the world.
   return <AnswerContext.Provider value={data?.today ?? ''}>{children}</AnswerContext.Provider>;
 }
